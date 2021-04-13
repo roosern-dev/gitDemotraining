60ac8b34bcc38b6c119e5f6e5b9b2d984b6a7893
Roo <rs.yeoh@estechs.com>
2021-04-13 13:32:04 +0800

    second commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var abc = "something";
+            var bgd = "soemthing thing";
+        }
+    }
+}
