4978e4db0907c84e7745f45987588f734640fac6
ShanyuEst <shanyu.chua.estechs@gmail.com>
2021-04-13 14:58:01 +0800

    -added 321

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!321");
 
             var bgd = "soemthing thing";
             var bsc = "khdakjdkgdk";
